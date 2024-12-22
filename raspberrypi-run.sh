05328ab8a20b04faafbc7b2ba62f396774b4d4b6
Dan Rummel <drummel@gmail.com>
2024-12-22 14:12:25 -0800

    Added setup and run scripts - fix sub dir for jar

--- raspberrypi-run.sh
+++ raspberrypi-run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Create a log filename
 LOGFILE=chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
--- raspberrypi-setup.sh
+++ raspberrypi-setup.sh
@@ -6,4 +6,4 @@ echo "Your authcode is: $liccode"
 wget https://github.com/heronarts/Chromatik/releases/download/1.0.1-SNAPSHOT-2024-11-11/Chromatik-1.0.1-SNAPSHOT-linux-aarch64.zip
 unzip Chromatik-1.0.1-SNAPSHOT-linux-aarch64.zip
 
-java -cp chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --authorize $liccode --accept-eula --require-license --headless
+java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --authorize $liccode --accept-eula --require-license --headless
