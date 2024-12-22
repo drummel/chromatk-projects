aa9d79943ad552828542cf54b456be23b36e7c1b
Dan Rummel <drummel@gmail.com>
2024-12-22 14:15:29 -0800

    Added setup and run scripts - fix sub dir on run sh

--- raspberrypi-run.sh
+++ raspberrypi-run.sh
@@ -2,4 +2,4 @@
 
 # Create a log filename
 LOGFILE=chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
-java -cp chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp  2>&1 | tee $LOGFILE
+java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp  2>&1 | tee $LOGFILE
