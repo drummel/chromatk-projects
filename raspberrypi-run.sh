23b1ea683d0c2a9cc640d7ca110db8a51a60513d
Dan Rummel <drummel@gmail.com>
2024-12-22 14:40:55 -0800

    Added dns routing when running the jar file

--- raspberrypi-run.sh
+++ raspberrypi-run.sh
@@ -2,4 +2,7 @@
 
 # Create a log filename
 LOGFILE=chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
-java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp  2>&1 | tee $LOGFILE
+java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp \
+-Dsun.net.dns.spi.nameservice.provider.1=dns,sun \
+-Dsun.net.dns.spi.nameservice.provider.2=mdns,sun \
+ 2>&1 | tee $LOGFILE
