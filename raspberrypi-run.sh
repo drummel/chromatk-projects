2ecedf4e6e2bccf5579ca4b1d5fbc941db8ec962
Dan Rummel <drummel@gmail.com>
2024-12-22 14:54:24 -0800

    Updead setup service - fix logging

--- raspberrypi-run.sh
+++ raspberrypi-run.sh
@@ -2,7 +2,13 @@
 
 # Create a log filename
 LOGFILE=chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
+
+# Use to run manualy and log
+# java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp \
+# -Dsun.net.dns.spi.nameservice.provider.1=dns,sun \
+# -Dsun.net.dns.spi.nameservice.provider.2=mdns,sun \
+#  2>&1 | tee $LOGFILE
+
 java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp \
--Dsun.net.dns.spi.nameservice.provider.1=dns,sun \
--Dsun.net.dns.spi.nameservice.provider.2=mdns,sun \
- 2>&1 | tee $LOGFILE
+ -Dsun.net.dns.spi.nameservice.provider.1=dns,sun \
+ -Dsun.net.dns.spi.nameservice.provider.2=mdns,sun
