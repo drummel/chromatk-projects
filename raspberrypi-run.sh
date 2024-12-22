f7615264fc6906e4608a58e7fa244e486f5dd135
Dan Rummel <drummel@gmail.com>
2024-12-22 15:07:54 -0800

    trying to fix path to java w/ hard code

--- raspberrypi-run.sh
+++ raspberrypi-run.sh
@@ -9,6 +9,6 @@ LOGFILE=$(pwd)/chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
 # -Dsun.net.dns.spi.nameservice.provider.2=mdns,sun \
 #  2>&1 | tee $LOGFILE
 
-java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp \
+/home/naga/.sdkman/candidates/java/current/bin/java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp \
  -Dsun.net.dns.spi.nameservice.provider.1=dns,sun \
  -Dsun.net.dns.spi.nameservice.provider.2=mdns,sun
