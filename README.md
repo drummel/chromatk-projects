86ffe094d6c8aa0b0a6c091029b8a850ed7b8e5d
Dan Rummel <drummel@gmail.com>
2024-12-22 16:11:26 -0800

    added scp command and ignore downloaded files

---
+++ README.md
@@ -0,0 +1,3 @@
+Copy all the files from your laptop to the pi:
+
+`scp -r ~/Chromatik naga@naga-pi.local:/home/naga/`
--- raspberrypi-run.sh
+++ raspberrypi-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a log filename with a timestamp
-LOGFILE=$(pwd)/chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
+# LOGFILE=$(pwd)/chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
 
 # Use to run manualy and log
 # java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp \
@@ -9,6 +9,9 @@ LOGFILE=$(pwd)/chromatik-run-$(date +"%Y-%m-%d_%H-%M-%S").log
 # -Dsun.net.dns.spi.nameservice.provider.2=mdns,sun \
 #  2>&1 | tee $LOGFILE
 
-/home/naga/.sdkman/candidates/java/current/bin/java -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp \
+/home/naga/.sdkman/candidates/java/current/bin/java \
+ -Dsun.net.inetaddr.ttl=0 \
  -Dsun.net.dns.spi.nameservice.provider.1=dns,sun \
- -Dsun.net.dns.spi.nameservice.provider.2=mdns,sun
+ -Dsun.net.dns.spi.nameservice.provider.2=mdns,sun \
+ -cp Chromatik-1.0.1-SNAPSHOT/chromatik-1.0.1-SNAPSHOT-linux-aarch64.jar heronarts.lx.studio.Chromatik \
+ --accept-eula --require-license --headless Projects/naga-cool-sparkle.lxp
